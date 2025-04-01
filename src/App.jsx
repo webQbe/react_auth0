358b768eb6c1a9f7688dacc2e3916495352b0c94
webQbe <webcubesolutionslk@gmail.com>
2025-04-01 14:35:50 +0530

    Handled authentication loading state and errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,35 @@
 import LoginButton from "./components/LoginButton"
 import LogoutButton from "./components/LogoutButton"
 import Profile from "./components/Profile"
+import { useAuth0 } from "@auth0/auth0-react"
 
 
 function App() {
+
+  // Get loading state & errors
+  const { isLoading, error } = useAuth0()
+
   return (
       <main className="column">
+
         <h1>Auth0 Login</h1>
-        <LoginButton /> {/* LoginButton for signing in */}
-        <LogoutButton /> {/* LogoutButton for signing out */}
-        <Profile /> {/* Show user information after login */}
+
+        { // Show error message if authentication fails
+          error && <p>Authentication Error</p> 
+        }
+
+        { isLoading ? ( // Show "Loading..." text while Auth0 is processing login
+
+          <p>Loading...</p> 
+
+        ) : ( // After Auth0 Loads (isLoading becomes false)
+          <>
+              <LoginButton /> {/* Render LoginButton (if not authenticated) */}
+              <LogoutButton /> {/* Render LogoutButton (if authenticated) */}
+              <Profile /> {/* Render Profile (if authenticated) */}
+          </>
+
+        )}
       </main>
   )
 }
